42cf06ae7e5511ba1eff41c09c8933cc9a1e9d8f
Tommi Kivimäki <tommi@tommikivimaki.com>
2018-11-28 13:29:38 +0200

    Initial Commit

---
+++ LeafHighlighter/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  LeafHighlighter
+//
+//  Created by Tommi Kivimäki on 28/11/2018.
+//  Copyright © 2018 Tommi Kivimäki. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+  func applicationDidFinishLaunching(_ aNotification: Notification) {
+    // Insert code here to initialize your application
+  }
+
+  func applicationWillTerminate(_ aNotification: Notification) {
+    // Insert code here to tear down your application
+  }
+
+
+}
+
---
+++ LeafHighlighter/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  LeafHighlighter
+//
+//  Created by Tommi Kivimäki on 28/11/2018.
+//  Copyright © 2018 Tommi Kivimäki. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    // Do any additional setup after loading the view.
+  }
+
+  override var representedObject: Any? {
+    didSet {
+    // Update the view, if already loaded.
+    }
+  }
+
+
+}
+
